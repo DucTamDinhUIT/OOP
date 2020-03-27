2d07ae40270d4881da1f012f9cfa74de1d3c79a7
Đinh Đức Tâm <ductam.astro@gmail.com>
2020-03-28 02:36:39 +0700

    Create Source.cpp

---
+++ 15520755.oop.lab1.5/Source.cpp
@@ -0,0 +1,16 @@
+//	OOP Lab1.5
+//	Source.cpp
+//	Purpose: Nhap ho&ten diemtoan diemvan, cout GPA= (dToan+dVan)/2
+//
+//	@author 15520755@gm.uit.edu.vn
+#include"Student.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	Student a;
+	a.nhapS();
+	a.gpaS();
+	a.xuatS();
+	return 1;
+}
