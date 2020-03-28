3da0db9ffaa315ef00ac0d513ba0635120855494
Đinh Đức Tâm <ductam.astro@gmail.com>
2020-03-29 02:25:56 +0700

    Update Date.h

--- 15520755.oop.lab1.4/Date.h
+++ 15520755.oop.lab1.4/Date.h
@@ -1,4 +1,7 @@
 #pragma once
+#ifndef DATE_H
+#define DATE_H
+
 #include <iostream>
 using namespace std;
 class Date
@@ -16,6 +19,16 @@ public:
 //	int xuatD();
 	friend istream& operator>>(istream &, Date &);
 	friend ostream& operator<<(ostream &, Date &);
+//	Date nextD();
+	int leapCheck();		//Check leapYear;
+	int dayCounter();		//count day of year;
+	int endOfYear();		//check 31/12;
+//	long number();
+//	int creat(int, int);
+//	int creat(long);
+
+	Date findDay();
 
 };
 
+#endif // COUNTER_H
