09f4de05033fbf0a196aa9c87774a48a94e1c3de
Đinh Đức Tâm <ductam.astro@gmail.com>
2020-03-29 02:25:59 +0700

    Update Source.cpp

--- 15520755.oop.lab1.4/Source.cpp
+++ 15520755.oop.lab1.4/Source.cpp
@@ -1,12 +1,23 @@
+//	@project: OOP Lab1.4
+//	@Source.cpp
+//	@purpose: Find the next day!
+//
+//	@author 15520755@gm.uit.edu.vn
 #include <iostream>
 #include "Date.h"
 using namespace std;
 int main()
 {
 	Date a;
-	cin >> a;
-	cout << a;
 //	a.nhapD();
+//	Date kq = a.nextD();
 //	a.xuatD();
+	cin >> a;
+	//Date kq = a.nextD();
+	cout << "hien thi leapYear: co(1)/khong(0): " << a.leapCheck() << endl;
+	cout << a.dayCounter() << endl; //tinh so thu tu ngay trong nam
+	cout << a.endOfYear() << endl;
+	cout << a;
+//	cout << kq;
 	return 1;
 }
